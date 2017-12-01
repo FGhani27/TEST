fc9fb62a8aae8996ef80a9009a912f4ff48a3417
Flow <Flow@FLOWG>
2017-12-02 03:23:41 +0700

    masih dalam pengetestan

---
+++ src/gym/Main.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+/**
+ *
+ * @author Flow
+ */
+public class Main {
+    
+    private String ktp;
+    private String nama;
+    private String cabang;
+    private String paket;
+
+    public String getKtp() {
+        return ktp;
+    }
+
+    public void setKtp(String ktp) {
+        this.ktp = ktp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+    
+    
+    
+}
